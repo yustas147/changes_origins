f47570cec2f2a6ae5460cefd40f9f2896a98a4ef
yustas147 <yustas@simbioz.com.ua>
2016-12-22 18:06:05 +0200

    1st

---
+++ __openerp__.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+{
+    'name': 'Page for origins display for changes',
+    'version': '8.0.1.0.0',
+    'author':   'Simbioz ',
+    'contributors': [
+        
+    ],
+    'website': 'http://simbioz.com.ua',
+    'category': 'Project Management',
+    'license': 'AGPL-3',
+    'summary': '''Page for origins display for changes on the form view of the model 
+                                                        change_management.change_form_view''',
+    'depends': [
+        'base',
+        'change_management',
+        'changes_origins'
+    ],
+    'demo': [],
+    'data': [
+        'view/change_form_view_inherited.xml'
+    ],
+    'test': [],
+    'js': [],
+    'css': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+}
